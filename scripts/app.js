85dcdc31cba1125eac6a2b1ca8b3de996a7d3b4f
bigulabot <111539473+bigulabot@users.noreply.github.com>
2025-05-30 23:01:41 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -216,3 +216,9 @@ window.onload = function() {
   updateDamageCounter();
   updateHP();
 }
+
+document.addEventListener('touchmove', function (event) {
+  if (event.touches.length > 1) {
+    event.preventDefault(); // Disable pinch zoom
+  }
+}, { passive: false });
